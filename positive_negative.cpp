c2c9da5c7eb916a72cede9d087749ca2c2ee868b
samruddhinagtilak <nagtilaksamruddhi24@gmail.com>
2026-08-28 12:25:02 +0530

    Create positive_negative.cpp

---
+++ positive_negative.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+
+cout<<"Enter A number=";
+if(n>0)
+  cout<<"Number is positive="<<n;
+else if(n<0)
+  cout<<"Number is Negative="<<n;
+else
+  cout<<"Number is Zero="<<n;
+
+return 0;
+
+}
