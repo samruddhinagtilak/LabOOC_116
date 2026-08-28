73cd477c371e1954327e59ad1f56061402196848
samruddhinagtilak <nagtilaksamruddhi24@gmail.com>
2026-08-28 12:19:30 +0530

    Create largest_three_number.cpp

---
+++ largest_three_number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+
+cout<<"Enter A three Numbers=";
+cin>>a>>b>>c;
+
+if(a>=b&& a>=c)
+  cout<<"largest="<<a;
+else if(b>=a&&b>=c)
+  cout<<"largest="<<b;
+esle
+  cout<<"largest="<<c;
+
+return 0;
+}
