46c78675f1d3dbb566b9fbb00e0a9702380e8225
samruddhinagtilak <nagtilaksamruddhi24@gmail.com>
2026-08-28 12:07:30 +0530

    Create even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+
+cout<<"Enter A Number = ";
+cin>>n;
+
+if(n % 2==0)
+    cout<<"Number is even";
+  else
+    cout<<"Numebr is Odd";
+
+  return 0;
+}
