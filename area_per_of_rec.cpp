3c64ef278c345482c3a0fce026c41eda2bc8552e
samruddhinagtilak <nagtilaksamruddhi24@gmail.com>
2026-08-28 11:47:49 +0530

    Create area_per_of_rec.cpp

---
+++ area_per_of_rec.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float length,width;
+  
+  cout<<"Enter length=";
+  cin>>length;
+
+  cout<<"Enter a width=";
+  cin>>width;
+
+  cout<<"Area = "<<length*width;
+  cout<<"\n Parameter="<<2*(length+width);
+  return 0;
+  
+}
