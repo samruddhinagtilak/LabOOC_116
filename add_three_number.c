3eab4b0c8ae0f076007cc66304510884dfc5194e
samruddhinagtilak <nagtilaksamruddhi24@gmail.com>
2026-08-29 11:56:36 +0530

    Create add_three_number.c

---
+++ add_three_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int a,b,c,sum;
+
+  printf("Enter Three Numbers=");
+  scanf("%d %d %d",&a,&b,&c);
+
+  sum = a+b+c;
+
+  printf("Addition = %d",sum);
+
+  return 0;
+}
